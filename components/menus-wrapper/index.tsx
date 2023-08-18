14eca1c64ee27b116db16b5d626ce4acec5848b1
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2023-08-18 17:31:56 +0330

    fix: src of mock data

--- components/menus-wrapper/index.tsx
+++ components/menus-wrapper/index.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { Box } from '@chakra-ui/react'
-import data from './data.json'
+import data from '../data.json'
 import { ParentMenu } from '../parent-menu'
 export const MenusWrapper = () => {
-  // console.log(data.data)
   return (
     <Box p='1' display='grid' gridTemplateColumns='repeat(2,1fr)' gap='8px'>
       {data.data.map((item) => (
