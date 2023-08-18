3b23ec8ac7db7ef300d2bf07c83f59ba4b170094
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2023-08-18 05:44:17 +0330

    feat: add `ParentMenu` component

---
+++ components/parent-menu/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Box, Text } from '@chakra-ui/react'
+
+export const ParentMenu = (props: any) => {
+  console.log(props)
+  const { thumbnail, title } = props.parentMenu
+  return (
+    <Box
+      backgroundImage={`url(${thumbnail.url})`}
+      backgroundPosition='center'
+      backgroundRepeat='no-repeat'
+      display='grid'
+      backgroundColor='grey'
+      backgroundBlendMode=' multiply'
+      height='150px'
+      placeItems='center'
+      borderRadius='12px'
+      overflow='hidden'
+    >
+      <Text>{title}</Text>
+    </Box>
+  )
+}
---
+++ components/parent-menu/types.ts
@@ -0,0 +1,7 @@
+export interface parentMenuProps {
+  parentMenu: {
+    imgSrc?: string
+    title: string
+    id: number
+  }
+}
