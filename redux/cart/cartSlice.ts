c2f1f4cfc5ac529693a458844401fd9d1df18815
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2023-08-18 15:31:04 +0330

    feat: add decrement reducer

--- redux/cart/cartSlice.ts
+++ redux/cart/cartSlice.ts
@@ -23,14 +23,24 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     increment: (state, action: PayloadAction<number>) => {
-      state.itemsOfCart[
-        state.itemsOfCart.findIndex((item) => item.id === action.payload)
-      ]
+      state.itemsOfCart[action.payload].count++
+    },
+    decrement: (state, action: PayloadAction<number>) => {
+      state.itemsOfCart[action.payload].count--
+      if (state.itemsOfCart[action.payload].count === 0) {
+        state.itemsOfCart = state.itemsOfCart
+          .slice(0, action.payload)
+          .concat(state.itemsOfCart.slice(action.payload + 1))
+      }
+    },
+    addToCart: (state, action: PayloadAction<cartType>) => {
+      state.itemsOfCart.push(action.payload)
+      state.count++
     },
   },
 })
 
-export const { increment } = cartSlice.actions
+export const { increment, addToCart, decrement } = cartSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCart = (state: RootState) => state.cart.itemsOfCart
