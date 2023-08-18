fb43315034b113d36eda9959434f82614bda0d6d
Pooya Sharifi <pooyasharifi.sp2@gmail.com>
2023-08-18 16:15:23 +0330

    feat: add `NotingFound` component

---
+++ components/noting-found/index.tsx
@@ -0,0 +1,7 @@
+'use client'
+import { Box, Text } from '@chakra-ui/react'
+export const NothingFound = () => (
+  <Box w='100%' h='70dvh' display='grid' placeItems='center'>
+    <Text>نتیجه‌ایی یافت نشد!</Text>
+  </Box>
+)
